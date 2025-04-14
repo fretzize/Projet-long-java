68cb35c5ca2561115c08f408d43917bfda782277
Noé Rigoletti <nono.rigoletti@gmail.com>
2025-04-14 18:32:21 +0200

    changements

--- core/src/main/java/projet/java/entite/sbire.java
+++ core/src/main/java/projet/java/entite/sbire.java
@@ -10,6 +10,11 @@ import java.util.List;
 
 public class sbire implements entite{
 
+    // Attributs de santé de l'entité sbire
+    int vie;
+    int bouclier;
+    int mana;
+
     //La cible du sbire
     private personnage cible;
 
@@ -21,19 +26,20 @@ public class sbire implements entite{
 
     //Texture du projectile lancé par le sbire
     private Texture projectileTexture;
-    private float porteeMax = 300f;
+    private float porteeMax;
     private int degats = 10;
     private float cooldown; // temps entre deux tirs (en secondes)
     private float tempsDepuisDernierTir = 0f;
     private Rectangle hitbox; // Hitbox du sbire
 
 
-    public sbire(float x, float y,float vitesseDeplacement,float cooldown,Rectangle hitbox,Texture projectileTexture) {
+    public sbire(float x, float y,float vitesseDeplacement,float cooldown,Rectangle hitbox, float porteeMax,Texture projectileTexture) {
         this.position = new Vector2(x, y);
         this.projectileTexture = projectileTexture;
         this.cooldown = cooldown;
         this.vitesseDeplacement = vitesseDeplacement;
         this.hitbox = hitbox;
+        this.porteeMax = porteeMax;
     }
 
     public void setCible(personnage cible) {
@@ -56,6 +62,25 @@ public class sbire implements entite{
         return this.position;
     }
 
+    public void augmenterMana(int mana){
+        this.mana += mana;
+    }
+
+    public void augmenterBouclier(int bouclier){
+        this.bouclier += bouclier;
+    }
+
+    public void augmenterVie(int vie){
+        this.vie += vie;
+    }
+
+    public void perdreBouclier(int valeur) {
+        this.bouclier -= valeur;
+        if (this.bouclier < 0) {
+            this.bouclier = 0;
+        }
+    }
+
     public boolean enVie(){
         if(this.getVie() > 0){
             return true;
@@ -63,18 +88,33 @@ public class sbire implements entite{
         return false;
     }
 
+    //Pour linstant ne sert pas a grand chose, mais apres y aura des sons et des animations quand il meurt
+    public void mourir() {
+        this.vie = 0;
+        this.bouclier = 0;
+        this.mana = 0;
+    }
+
 
     public void update(float delta, List<Projectile> projectiles) {
         tempsDepuisDernierTir += delta;
 
-        if (cible != null && peutAttaquer()) {
+        if (cible != null && peutAttaquer() && estAPortee()) {
             tirerSurCible(projectiles);
             tempsDepuisDernierTir = 0;
         }
     }
 
+    // Vérifie si le sbire peut tirer (si le cooldown est écoulé)
     private boolean peutAttaquer() {
         return tempsDepuisDernierTir >= cooldown;
+    }
+
+    // Vérifie si la cible est à portée (si la distance est inférieure à la portée maximale)
+    private boolean estAPortee() {
+        if (cible == null) return false;
+        float distance = position.dst(cible.getPosition());
+        return distance <= porteeMax;
     }
 
     private void tirerSurCible(List<Projectile> projectiles) {
@@ -100,15 +140,14 @@ public class sbire implements entite{
 
     public void prendreDegats(int degats) {
         this.vie -= degats;
-        if (this.vie < 0) {
-            this.vie = 0; // Assurez-vous que la vie ne devienne pas négative
+        if (this.vie <= 0) {
+            mourir();
         }
     }
 
     public Rectangle getHitbox() {
         return hitbox;
     }
-
 
     public void deplacer(float deltaTime) {
         if (cible != null) {
