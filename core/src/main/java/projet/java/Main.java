2f7724ae215bb84b431351f2a0ff426442608d75
Mathis Magron <mathis.magron@laposte.net>
2025-04-14 14:22:50 +0200

    test

--- core/src/main/java/projet/java/Main.java
+++ core/src/main/java/projet/java/Main.java
@@ -1,34 +1,50 @@
 package projet.java;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+//import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+
+import projet.java.Menu.Menu;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class Main extends Game {
+
+    public SpriteBatch batch;
+    public FitViewport viewport;
+    public BitmapFont font;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        viewport = new FitViewport(1920, 1080);
+        font = new BitmapFont();
+
+        // Charger une police vectorielle
+//        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("police.otf"));
+//        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+//        parameter.size = 48; // Taille de la police
+//        font = generator.generateFont(parameter);
+        //generator.dispose(); // Libérer les ressources du générateur
+
+
+
+
+        font.setUseIntegerPositions(false);
+        font.getData().setScale(viewport.getWorldHeight() / Gdx.graphics.getHeight());
+        this.setScreen(new Menu(this));
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        super.render();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        font.dispose();
     }
 }
--- core/src/main/java/projet/java/Menu/Menu.java
+++ core/src/main/java/projet/java/Menu/Menu.java
@@ -1,4 +1,125 @@
 package projet.java.Menu;
 
-public class Menu {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+
+import projet.java.Main;
+
+public class Menu implements Screen {
+    
+    final Main game;
+    private Texture backgroundTexture;
+    private int selectedIndex = 0; // Index de l'option sélectionnée
+    private final String[] menuOptions = {"Commencer la partie", "Options", "Quitter"};
+
+    public Menu(final Main game) {
+        this.game = game;
+    }
+
+    @Override
+    public void renderMenu(float delta) {
+        ScreenUtils.clear(Color.BLACK);
+
+        game.viewport.apply();
+        game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
+
+        game.batch.begin(); // Assurez-vous que le batch commence avant tout dessin
+
+        // Calculer les dimensions de l'écran
+        float screenWidth = game.viewport.getWorldWidth();
+        float screenHeight = game.viewport.getWorldHeight();
+
+        game.batch.draw(backgroundTexture, 0, 0, game.viewport.getWorldWidth(), game.viewport.getWorldHeight());
+
+        // Ajuster la taille de la police pour agrandir les options du menu
+        game.font.getData().setScale(3.0f); // Augmentez l'échelle selon vos besoins
+
+        // Afficher les options du menu
+        for (int i = 0; i < menuOptions.length; i++) {
+            String option = menuOptions[i];
+            float textWidth = game.font.getRegion().getRegionWidth();
+            float x = (screenWidth - textWidth) / 2;
+            float y = screenHeight / 2 - i * 50; // Espacement entre les options
+
+            // Changer la couleur pour l'option sélectionnée
+            if (i == selectedIndex) {
+                game.font.setColor(Color.YELLOW);
+            } else {
+                game.font.setColor(Color.WHITE);
+            }
+        
+            game.font.draw(game.batch, option, x, y);
+        }
+
+        game.batch.end(); // Assurez-vous que le batch se termine après tout dessin
+
+        // Gérer les entrées clavier
+        if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+            selectedIndex = (selectedIndex - 1 + menuOptions.length) % menuOptions.length;
+        } else if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
+            selectedIndex = (selectedIndex + 1) % menuOptions.length;
+        } else if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+            handleMenuSelection();
+        }
+    }
+
+    private void handleMenuSelection() {
+        switch (selectedIndex) {
+            case 0:
+                game.setScreen(new GameScreen(game));
+                dispose();
+                break;
+            case 1:
+                // Implémenter l'écran des options
+                // game.setScreen(new OptionsScreen(game));
+                // dispose();
+                // Pour l'instant, on va juste afficher un message dans la console
+                System.out.println("Options sélectionnées !");
+                
+                break;
+            case 2:
+                Gdx.app.exit();
+                break;
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        game.viewport.update(width, height, true);
+    }
+
+    @Override
+    public void show() {
+        backgroundTexture = new Texture(Gdx.files.internal("menubackground.png"));
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void dispose() {
+        if (backgroundTexture != null) {
+            backgroundTexture.dispose();
+        }
+    }
 }
