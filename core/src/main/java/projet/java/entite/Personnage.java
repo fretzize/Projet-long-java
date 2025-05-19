f88daeae71067568a51a4dfea20a6969ec315478
alexandre <alex.amedro@gmail.com>
2025-05-20 00:37:05 +0200

    majuscule + collision map et perso principal

--- core/src/main/java/projet/java/entite/Personnage.java
+++ core/src/main/java/projet/java/entite/Personnage.java
@@ -5,19 +5,10 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.Disposable;
 import projet.java.Main;
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.Vector;
 
 
 public class Personnage extends ApplicationAdapter implements Entite {
